9ae595c49acfc6e7dfd90ec4d1a5457ce749a31e
Tobias Gesellchen <tobias@gesellix.de>
2018-03-25 16:50:59 +0200

    gracefully handle existing users and databases, also skip cluster config when `cluster_finished`.

--- pkg/cluster-setup.go
+++ pkg/cluster-setup.go
@@ -3,6 +3,7 @@ package cluster_config
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -17,29 +18,68 @@ type ClusterSetup struct {
 	NodeCount   int    `json:"node_count,omitempty"`
 }
 
+type ClusterSetupResponse struct {
+	State string `json:"state"`
+}
+
+func AdminExists(ip string, auth BasicAuth, insecure bool) (bool, error) {
+	client := NewCouchdbClient(fmt.Sprintf("http://%s:5984", ip), BasicAuth{}, insecure)
+	resp, err := client.Request(
+		"POST",
+		fmt.Sprintf("%s/_session", client.BaseUri),
+		strings.NewReader(fmt.Sprintf("{\"name\":\"%s\",\"password\":\"%s\"}", auth.Username, auth.Password)))
+	if err != nil {
+		return false, err
+	}
+	return resp.StatusCode == 200, nil
+}
+
 func CreateAdmin(ipAddresses []string, auth BasicAuth, insecure bool) error {
 	for _, ip := range ipAddresses {
-		client := NewCouchdbClient(fmt.Sprintf("http://%s:5984", ip), BasicAuth{}, insecure)
-		_, err := client.Request(
-			"PUT",
-			fmt.Sprintf("%s/_node/couchdb@%s/_config/admins/%s", client.BaseUri, ip, auth.Username),
-			strings.NewReader(fmt.Sprintf("\"%s\"", auth.Password)))
-		// TODO when the admin already exists, then don't fail.
-		if err != nil {
-			return err
+		if ok, err := AdminExists(ip, auth, insecure); !ok {
+			if err != nil {
+				return err
+			}
+			client := NewCouchdbClient(fmt.Sprintf("http://%s:5984", ip), BasicAuth{}, insecure)
+			_, err = client.Request(
+				"PUT",
+				fmt.Sprintf("%s/_node/couchdb@%s/_config/admins/%s", client.BaseUri, ip, auth.Username),
+				strings.NewReader(fmt.Sprintf("\"%s\"", auth.Password)))
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
+}
+
+func DatabaseExists(dbName string, ip string, auth BasicAuth, insecure bool) (bool, error) {
+	client := NewCouchdbClient(fmt.Sprintf("http://%s:5984", ip), auth, insecure)
+	resp, err := client.Request(
+		"GET",
+		fmt.Sprintf("%s/%s", client.BaseUri, dbName),
+		nil)
+	if err != nil {
+		return false, err
+	}
+	return resp.StatusCode == 200, nil
 }
 
 func CreateCoreDatabases(databaseNames []string, ipAddresses []string, auth BasicAuth, insecure bool) error {
 	for _, ip := range ipAddresses {
 		client := NewCouchdbClient(fmt.Sprintf("http://%s:5984", ip), auth, insecure)
 		for _, dbName := range databaseNames {
-			_, err := client.Request("PUT", fmt.Sprintf("%s/%s", client.BaseUri, dbName), nil)
-			// TODO when the database already exists, then don't fail.
-			if err != nil {
-				return err
+			if ok, err := DatabaseExists(dbName, ip, auth, insecure); !ok {
+				if err != nil {
+					return err
+				}
+				_, err := client.Request(
+					"PUT",
+					fmt.Sprintf("%s/%s", client.BaseUri, dbName),
+					nil)
+				if err != nil {
+					return err
+				}
 			}
 		}
 	}
@@ -66,11 +106,32 @@ func SetupClusterNodes(ipAddresses []string, adminAuth BasicAuth, insecure bool)
 		return err
 	}
 
-	setupNodeIp := ipAddresses[:1]
+	setupNodeIp := ipAddresses[:1][0]
 	otherNodeIps := ipAddresses[1:]
 	nodeCount := len(ipAddresses)
 
 	client := NewCouchdbClient(fmt.Sprintf("http://%s:5984", setupNodeIp), adminAuth, insecure)
+
+	resp, err := client.Request("GET",
+		fmt.Sprintf("http://%s:5984/_cluster_setup", setupNodeIp),
+		nil)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode == 200 {
+		respBody, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+
+		var clusterSetupResponse ClusterSetupResponse
+		err = json.Unmarshal(respBody, &clusterSetupResponse)
+		if err != nil {
+			return err
+		}
+		if clusterSetupResponse.State == "cluster_finished" {
+			// cluster already set up
+			return nil
+		}
+	}
 
 	body, err := json.Marshal(ClusterSetup{
 		Action:      "enable_cluster",
@@ -81,7 +142,13 @@ func SetupClusterNodes(ipAddresses []string, adminAuth BasicAuth, insecure bool)
 	if err != nil {
 		return err
 	}
-	client.Request("POST", fmt.Sprintf("http://%s:5984/_cluster_setup", setupNodeIp), strings.NewReader(string(body)))
+	_, err = client.Request(
+		"POST",
+		fmt.Sprintf("http://%s:5984/_cluster_setup", setupNodeIp),
+		strings.NewReader(string(body)))
+	if err != nil {
+		return err
+	}
 
 	for _, ip := range otherNodeIps {
 		body, err = json.Marshal(ClusterSetup{
@@ -95,7 +162,13 @@ func SetupClusterNodes(ipAddresses []string, adminAuth BasicAuth, insecure bool)
 		if err != nil {
 			return err
 		}
-		client.Request("POST", fmt.Sprintf("http://%s:5984/_cluster_setup", setupNodeIp), strings.NewReader(string(body)))
+		_, err = client.Request(
+			"POST",
+			fmt.Sprintf("http://%s:5984/_cluster_setup", setupNodeIp),
+			strings.NewReader(string(body)))
+		if err != nil {
+			return err
+		}
 
 		body, err = json.Marshal(ClusterSetup{
 			Action:   "add_node",
@@ -106,7 +179,13 @@ func SetupClusterNodes(ipAddresses []string, adminAuth BasicAuth, insecure bool)
 		if err != nil {
 			return err
 		}
-		client.Request("POST", fmt.Sprintf("http://%s:5984/_cluster_setup", setupNodeIp), strings.NewReader(string(body)))
+		_, err = client.Request(
+			"POST",
+			fmt.Sprintf("http://%s:5984/_cluster_setup", setupNodeIp),
+			strings.NewReader(string(body)))
+		if err != nil {
+			return err
+		}
 	}
 
 	body, err = json.Marshal(ClusterSetup{
@@ -114,7 +193,13 @@ func SetupClusterNodes(ipAddresses []string, adminAuth BasicAuth, insecure bool)
 	if err != nil {
 		return err
 	}
-	client.Request("POST", fmt.Sprintf("http://%s:5984/_cluster_setup", setupNodeIp), strings.NewReader(string(body)))
+	_, err = client.Request(
+		"POST",
+		fmt.Sprintf("http://%s:5984/_cluster_setup", setupNodeIp),
+		strings.NewReader(string(body)))
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
